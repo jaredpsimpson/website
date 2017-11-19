40228ef457465c8d649f08949397cf87637b6e63
Sean Sparkman <sean.sparkman@outlook.com>
2017-11-18 18:10:46 -0600

    remove forcing the non custom error page

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace devfestweekend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (true && env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
