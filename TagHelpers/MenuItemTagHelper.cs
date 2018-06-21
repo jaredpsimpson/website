d76f6f87a919e5bf7defc27c79a5cf443213d19c
Sean Sparkman <sean.sparkman@outlook.com>
2018-06-20 21:37:48 -0400

    Tag Helpers for Menu Items

---
+++ TagHelpers/MenuItemTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devfestweekend.TagHelpers
+{
+    [HtmlTargetElement(Attributes = nameof(Href), ParentTag = "menu")]
+    public class MenuItemTagHelper : TagHelper
+    {
+        public string Href { get; set; }
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await Task.Run(() =>
+            {
+                output.TagName = "li";
+                var hrefPath = new PathString(Href);
+                var currentPath = new PathString((string)context.Items["path"]);
+                if (hrefPath.Equals(currentPath))
+                {
+                    output.Attributes.Add("class", "active");
+                }
+                output.PreContent.SetHtmlContent($"<a href=\"{Href}\">");
+                output.PostContent.SetHtmlContent("</a>");
+            });
+        }
+    }
+}
---
+++ TagHelpers/MenuTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devfestweekend.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "Path")]
+    public class MenuTagHelper : TagHelper
+    {
+        public PathString Path { get; set; }
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await Task.Run(() =>
+            {
+                // provide the path to menu-items
+                context.Items.Add("path", Path.Value);
+
+                output.TagName = "ul";
+                output.Attributes.Add("class", "nav navbar-nav");
+            });
+        }
+    }
+}
