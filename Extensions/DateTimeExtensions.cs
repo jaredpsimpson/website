8c5ad89c5a1f6f99ed5122c3a64c4d0fdc62d007
Sean Sparkman <sean.sparkman@outlook.com>
2018-07-16 12:57:00 -0500

    Added timezone conversion

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devfestweekend.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        private static readonly DateTimeZone CentralTimeZone =
+                DateTimeZoneProviders.Tzdb["US/Central"];
+        public static DateTime ConvertToCentral(this DateTime date)
+        {
+            return Instant.FromDateTimeUtc(date)
+                .InZone(CentralTimeZone)
+                .ToDateTimeUnspecified();
+        }
+    }
+}
