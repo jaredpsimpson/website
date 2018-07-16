d3c37e180c1ef7d7a3253ccbdf5d4588730af2ed
Sean Sparkman <sean.sparkman@outlook.com>
2018-07-16 12:59:39 -0500

    added upcoming dates and featured speakers

---
+++ Models/Date.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devfestweekend.Models
+{
+    public class Date
+    {
+        public DateTime When { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public string Href { get; set; }
+        public string CallToAction { get; set; }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using devfestweekend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,10 +10,45 @@ namespace devfestweekend.Pages
 {
     public class IndexModel : PageModel
     {
+        public IndexModel()
+        {
+            Speakers = new List<Speaker>();
+            Dates = new List<Date>();
+        }
+        public List<Speaker> Speakers { get; set; }
+        public List<Date> Dates { get; set; }
 
         public void OnGet()
         {
-            //PaperCall = new _EventModel { Title = "Papercall", SubTitle = "Talk Submission Due Date", Date = new DateTime(2018, 8, 13), LinkText = "Submit Talk", LinkUrl = "https://papercall.io" };
+            Speakers = new List<Speaker>
+            {
+                //new Speaker
+                //{
+                //    FirstName = "Billy",
+                //    LastName = "Hollys",
+                //    AvatarUrl = "images/speaker/8.jpg",
+                //    Biography = "Elevators!"
+                //}
+            };
+
+            Dates = new List<Date>
+            {
+                new Date
+                {
+                    Href = "https://www.papercall.io/devfestweekend",
+                    When = DateTime.Parse("2018-08-17 00:00:00-05:00"),
+                    Title = "End of Call for Speakers",
+                    CallToAction = "Submit Paper"
+                },
+                new Date
+                {
+                    Href = "https://devfestweekend.eventbrite.com/",
+                    When = DateTime.Parse("2018-10-13 09:00:00-05:00"),
+                    Title = "Irving, TX",
+                    CallToAction = "Get Tickets",
+                    Address = "7000 State Highway 161, Irving, TX"
+                }
+            };
         }
     }
 }
