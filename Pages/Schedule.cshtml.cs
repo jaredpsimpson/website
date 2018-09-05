594a28d55473a9d35a28ca2fd56f758660ccc806
Sean Sparkman <sean.sparkman@outlook.com>
2018-09-05 14:24:55 -0500

    schedule

--- Pages/Schedule.cshtml.cs
+++ Pages/Schedule.cshtml.cs
@@ -16,12 +16,52 @@ namespace devfestweekend.Pages
     {
         private DataService dataService = new DataService();
         public List<FeaturedEvent> Events { get; set; }
-        public List<Session> Sessions { get; set; }
+        public List<RoomSessions> Tracks { get; set; }
+        public List<string> Tabs { get; set; }
+            = new List<string>
+            {
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven"
+            };
 
         public async Task OnGet()
         {
+            var sessions = await dataService.GetSessionsAsync();
+            var rooms = sessions.Select(s => s.Room).Distinct(new RoomComparer());
+
+            sessions = sessions.OrderBy(s => s.Room.Id).ThenBy(s => s.StartTime).ToList();
+
             Events = await dataService.GetEventsAsync();
-            Sessions = await dataService.GetSessionsAsync();
+            Tracks = (from session in sessions
+                      group session by session.Room.Id into track
+                      select new RoomSessions
+                      {
+                        Room = rooms.First(r => r.Id == track.Key),
+                        Sessions = track.ToList()
+                      }).ToList();
+        }
+    }
+
+    public class RoomSessions
+    {
+        public Room Room { get; set; }
+        public List<Session> Sessions { get; set; }
+    }
+
+    public class RoomComparer : IEqualityComparer<Room>
+    {
+        public bool Equals(Room x, Room y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(Room obj)
+        {
+            return obj.Id.GetHashCode();
         }
     }
 }
